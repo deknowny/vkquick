de83b9872a2237010cc867fd03a731ea3e9a3832
Rhinik <deknowny@gmail.com>
2020-06-18 15:58:25 +0300

    action validator

---
+++ vkquick/validators/action.py
@@ -0,0 +1,23 @@
+from .base import Validator
+from vkquick.annotypes import Annotype
+
+
+class Action(Validator, Annotype):
+    """
+    Reaction to some message new actions
+    """
+
+    def __init__(self, *types):
+        self.types = types
+
+    @staticmethod
+    def prepare(argname, event, func, bot, bin_stack):
+        return event.object.message.action
+
+    def isvalid(self, event, com, bot, bin_stack):
+        if (
+            "action" in event.object.message and
+            event.object.message.action.type in self.types
+        ):
+            return True
+        return False
