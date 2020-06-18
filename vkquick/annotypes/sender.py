c5a6dd273cd848dfb18404eaee22ddd9b6877193
Rhinik <deknowny@gmail.com>
2020-06-19 00:37:48 +0300

    Fix static

--- vkquick/annotypes/sender.py
+++ vkquick/annotypes/sender.py
@@ -6,8 +6,7 @@ class Sender(Annotype, UserAnno):
     """
     User that sent a message
     """
-    @staticmethod
-    async def prepare(argname, event, func, bot, bin_stack):
+    async def prepare(self, argname, event, func, bot, bin_stack):
         return await User(
             user_id=event.object.message.from_id
         ).get_info(bot.api, *self.fields)
