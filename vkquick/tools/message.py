5b2076e0f014e078b261d91ffeede358c82e8a3e
Rhinik <deknowny@gmail.com>
2020-06-18 22:38:20 +0300

    Commas deleted

--- vkquick/tools/message.py
+++ vkquick/tools/message.py
@@ -8,26 +8,26 @@ class Message:
     Return a message in your command
     with full `messages.send` parameters
     """
-    message: Optional[int] = None,
-    peer_id: Optional[int] = None,
-    random_id: Optional[int] = None,
-    user_id: Optional[int] = None,
-    domain: Optional[str] = None,
-    chat_id: Optional[int] = None,
-    user_ids: Optional[List[int]] = None,
-    lat: Optional[float] = None,
-    long: Optional[float] = None,
-    attachment: Optional[List[str]] = None,
-    reply_to: Optional[int] = None,
-    forward_messages: Optional[List[int]] = None,
-    sticker_id: Optional[int] = None,
-    group_id: Optional[int] = None,
-    keyboard: Optional[str] = None,
-    payload: Optional[str] = None,
-    dont_parse_links: Optional[bool] = None,
-    disable_mentions: Optional[bool] = None,
-    intent: Optional[str] = None,
-    expire_ttl: Optional[int] = None,
+    message: Optional[int] = None
+    peer_id: Optional[int] = None
+    random_id: Optional[int] = None
+    user_id: Optional[int] = None
+    domain: Optional[str] = None
+    chat_id: Optional[int] = None
+    user_ids: Optional[List[int]] = None
+    lat: Optional[float] = None
+    long: Optional[float] = None
+    attachment: Optional[List[str]] = None
+    reply_to: Optional[int] = None
+    forward_messages: Optional[List[int]] = None
+    sticker_id: Optional[int] = None
+    group_id: Optional[int] = None
+    keyboard: Optional[str] = None
+    payload: Optional[str] = None
+    dont_parse_links: Optional[bool] = None
+    disable_mentions: Optional[bool] = None
+    intent: Optional[str] = None
+    expire_ttl: Optional[int] = None
     silent: Optional[bool] = None
 
 
