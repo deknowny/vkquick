367275dca3481614e19804e0ac9a2969ee2a1d55
Rhinik <deknowny@gmail.com>
2020-06-27 21:21:35 +0300

    New struct

--- vkquick/reaction.py
+++ vkquick/reaction.py
@@ -10,11 +10,12 @@ import click
 
 from . import annotypes
 from . import tools
+from . import current
 
 
 class Reaction:
     """
-    LongPoll's events handler
+    Обработчик _корректного_ события LongPoll.ы
     """
 
     def __init__(
@@ -67,7 +68,7 @@ class Reaction:
 
     async def run(self, comkwargs):
         if icf(self.code):
-            return await create_task(self.code(**comkwargs))
+            return await self.code(**comkwargs)
         else:
             return self.code(**comkwargs)
 
@@ -88,7 +89,7 @@ class Reaction:
 
 class ReactionsList(list):
     """
-    List with events handler
+    Список с обработчиками корректных событий
     """
     def has_event(self, event_name):
         for reaction in self:
@@ -98,7 +99,7 @@ class ReactionsList(list):
 
         return False
 
-    async def _send_message(self, api, event, message):
+    async def _send_message(self, event, message):
         """
         Send a meessage by user's returning in reaction
         """
@@ -106,13 +107,13 @@ class ReactionsList(list):
             if message.params.peer_id is Ellipsis:
                 message.params.peer_id = event.object.message.peer_id
 
-            await api.messages.send(
+            await current.api.messages.send(
                 **message.params
             )
         elif message is None:
             return
         else:
-            await api.messages.send(
+            await current.api.messages.send(
                 random_id=0,
                 peer_id=event.object.message.peer_id,
                 message=str(message)
@@ -123,13 +124,82 @@ class ReactionsList(list):
         print("Show full event? (key)")
         return click.getchar()
 
-    async def resolve(self, event, bot):
+    async def devalidate(self, event, reaction, bin_stack):
+        TEXT = click.style(reaction.code.__name__, fg="cyan") + "\n"
+
+        for validator in reaction.validators:
+            if icf(validator.isvalid):
+                val = await validator.isvalid(
+                    event, reaction, bin_stack
+                )
+
+            else:
+                val = validator.isvalid(
+                    event, reaction, bin_stack
+                )
+
+            if not val[0]:
+                TEXT += f"-- {validator.__class__.__name__}: " +\
+                    click.style(
+                        "not valid\n", fg="red"
+                    )
+
+                TEXT += "   -> " +\
+                    click.style(
+                        val[1] + "\n", fg="red"
+                    )
+
+                current.bot.debug_out(TEXT)
+                break
+
+            TEXT += f"-- {validator.__class__.__name__}: " +\
+                click.style(
+                    "valid\n", fg="green"
+                )
+
+        else:
+            comkwargs = {}
+            for name, value in reaction.args.items():
+                if icf(value.prepare):
+                    content = await value.prepare(
+                        argname=name,
+                        event=event,
+                        func=reaction,
+                        bin_stack=bin_stack
+                    )
+                else:
+                    content = value.prepare(
+                        argname=name,
+                        event=event,
+                        func=reaction,
+                        bin_stack=bin_stack
+                    )
+                comkwargs.update({name: content})
+
+
+            for key, value in comkwargs.items():
+                TEXT += "> " +\
+                    click.style(key, fg="yellow") +\
+                    f" = {value!r}\n"
+            current.bot.debug_out(TEXT)
+            response = await reaction.run(comkwargs)
+
+            if isgeneratorfunction(reaction.code):
+                await self._send_message(
+                    current.bot.api, event, "".join(response)
+                )
+            else:
+                await self._send_message(event, response)
+
+    async def resolve(self, event):
         header_printed = False
 
         for reaction in self:
+
             if event.type in reaction.events_name or reaction.events_name is Ellipsis:
+
                 if not header_printed:
-                    bot.debug_out(
+                    current.bot.debug_out(
                         click.style("[Reactions on ", bold=True) +\
                         click.style(event.type, fg="cyan") +\
                         "]" +\
@@ -141,72 +211,6 @@ class ReactionsList(list):
                 header_printed = True
                 # Class for escaping race condition
                 bin_stack = type("BinStack", (), {})
-                bot.debug_out(click.style(reaction.code.__name__, fg="cyan"))
-                for validator in reaction.validators:
-                    if icf(validator.isvalid):
-                        val = await validator.isvalid(
-                            event, reaction, bot, bin_stack
-                        )
-
-                    else:
-                        val = validator.isvalid(
-                            event, reaction, bot, bin_stack
-                        )
-
-                    if not val[0]:
-                        bot.debug_out(
-                            f"-- {validator.__class__.__name__}: " +\
-                            click.style(
-                                "not valid", fg="red"
-                            )
-                        )
-                        bot.debug_out(
-                            "   -> " +\
-                            click.style(
-                                val[1], fg="red"
-                            ), end="\n\n"
-                        )
-                        break
-                    bot.debug_out(
-                        f"-- {validator.__class__.__name__}: " +\
-                        click.style(
-                            "valid", fg="green"
-                        )
-                    )
-                else:
-                    comkwargs = {}
-                    for name, value in reaction.args.items():
-                        if icf(value.prepare):
-                            content = await value.prepare(
-                                argname=name,
-                                event=event,
-                                func=reaction,
-                                bot=bot,
-                                bin_stack=bin_stack
-                            )
-                        else:
-                            content = value.prepare(
-                                argname=name,
-                                event=event,
-                                func=reaction,
-                                bot=bot,
-                                bin_stack=bin_stack
-                            )
-                        comkwargs.update({name: content})
-
-
-                    for key, value in comkwargs.items():
-                        bot.debug_out(
-                            "> " +\
-                            click.style(key, fg="yellow") +\
-                            f" = {value!r}"
-                        )
-                    bot.debug_out("")
-                    response = await reaction.run(comkwargs)
-
-                    if isgeneratorfunction(reaction.code):
-                        await self._send_message(
-                            bot.api, event, "".join(response)
-                        )
-                    else:
-                        await self._send_message(bot.api, event, response)
+                create_task(
+                    self.devalidate(event, reaction, bin_stack)
+                )
