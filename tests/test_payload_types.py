0e4b359f4249d46091dc5e513e369e4ddd72c8e4
Rhinik <deknowny@gmail.com>
2020-07-11 21:57:20 +0300

    Testing payloads as unit

---
+++ tests/test_payload_types.py
@@ -0,0 +1,36 @@
+"""
+In process
+"""
+# import copy
+#
+# import vkquick as vq
+# import pytest
+#
+# import tests.events as events
+#
+#
+# @pytest.mark.asyncio
+# async def test_payload_users():
+#     user_id = 1
+#     event = copy.deepcopy(
+#         events.SIMPLE_MESSAGE
+#     )
+#
+#     event.object.message.from_id = user_id
+#
+#     # Custom
+#     user = await vq.User(user_id=user_id).get_info(
+#         "bdate", name_case="acc"
+#     )
+#
+#     # Via Sender
+#     sender = vq.Sender("bdate", name_case="acc")
+#     sender = await sender.prepare(
+#         argname=...,
+#         event=event,
+#         func=...,
+#         bin_stack=...
+#     )
+#     assert sender.id == user.id
+#     assert sender.fn == user.fn
+#     assert sender.info == user.info
