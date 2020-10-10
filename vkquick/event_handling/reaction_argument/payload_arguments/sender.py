b61b1567b5a6eeb921c22b2785e2a319cdb2991f
Rhinik <deknowny@gmail.com>
2020-10-10 22:39:40 +0300

    New Sender type

---
+++ vkquick/event_handling/reaction_argument/payload_arguments/sender.py
@@ -0,0 +1,17 @@
+import typing as ty
+
+import vkquick.event_handling.reaction_argument.payload_arguments.base
+import vkquick.events_generators.event
+import vkquick.wrappers.user
+
+
+class Sender(vkquick.event_handling.reaction_argument.payload_arguments.base.PayloadArgument):
+
+    async def init_value(
+        self, event: vkquick.events_generators.event.Event
+    ) -> ty.Any:
+        sender_id = event.get_message_object().from_id
+        user = await vkquick.wrappers.user.User.build_from_id(sender_id)
+        return user
+
+
