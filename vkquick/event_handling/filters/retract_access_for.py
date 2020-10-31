f4722de88d70a30ada0dbad492fed9ac65b7cd4f
HateMe02 <64659298+HateMe02@users.noreply.github.com>
2020-10-31 07:51:02 -0700

    Create RetractAccessFor filter

---
+++ vkquick/event_handling/filters/retract_access_for.py
@@ -0,0 +1,23 @@
+import typing as ty
+
+import vkquick.event_handling.filters.base
+import vkquick.events_generators.event
+
+
+class RetractAccessFor(vkquick.event_handling.filters.base.Filter):
+
+    passed_decision = "Пользователь не имеет ограничение к команде"
+    not_passed_decision = "Пользователь имеет ограничение к команде"
+
+    def __init__(self, *ids):
+        self.ids = ids
+
+    def make_decision(
+            self, event: vkquick.events_generators.event.Event
+    ) -> ty.Tuple[bool, str]:
+        """
+        Проверяет на доступ к команде
+        """
+        if event.get_message_object().from_id not in self.ids:
+            return True, self.passed_decision
+        return False, self.not_passed_decision
