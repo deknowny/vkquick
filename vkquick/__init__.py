5fbe1b8af2d84848b6a7f119bc524299527f2bed
Rhinik <deknowny@gmail.com>
2020-10-31 23:12:49 +0300

    Add import from filter.py

--- vkquick/__init__.py
+++ vkquick/__init__.py
@@ -31,13 +31,13 @@ from .json_parsers import BuiltinJSONParser
 from .events_generators.event import Event
 from .events_generators.longpoll import GroupLongPoll, UserLongPoll
 
-from vkquick.event_handling.payload_arguments.message import Message
+from vkquick.event_handling.message import Message
 from .event_handling.command import Command
 from .event_handling.event_handler import EventHandler
 from .event_handling.handling_info_scheme import HandlingInfoScheme
 
-from vkquick.base.text_argument import TextArgument
-from vkquick.base.text_argument import UnmatchedArgument
+from .base.text_argument import TextArgument
+from .base.text_argument import UnmatchedArgument
 from .event_handling.text_arguments.integer import Integer
 from .event_handling.text_arguments.word import Word
 from .event_handling.text_arguments.string_ import String
@@ -47,15 +47,16 @@ from .event_handling.text_arguments.bool_ import Bool
 from .event_handling.text_arguments.list_ import List
 from .event_handling.text_arguments.mention import UserMention
 from .event_handling.text_arguments.text_values_base import TextBase
+from .event_handling.text_arguments.optional import Optional
 
 from vkquick.base.payload_argument import PayloadArgument
 from .event_handling.payload_arguments.sender import Sender
 from .event_handling.payload_arguments.replied_user import RepliedUser
 from .event_handling.payload_arguments.current import Current
 from .event_handling.payload_arguments.captured_event import CapturedEvent
-from .event_handling.payload_arguments._answer import Answer
+from .event_handling.payload_arguments.answer import Answer
 
-from vkquick.base.filter import Filter
+from .base.filter import Filter, FilterResponse, Decision, DecisionStatus
 from .event_handling.filters.enable import Enable, EnableStatus
 
 from .wrappers.user import User
