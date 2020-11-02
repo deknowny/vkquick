30632282d651b4e2bae2991c4d6596bbf0c95412
HateMe02 <godmodeon34567@gmail.com>
2020-11-01 22:26:10 -0800

    Update ChatOnly filter to new syntax

--- vkquick/event_handling/filters/chat_only.py
+++ vkquick/event_handling/filters/chat_only.py
@@ -1,17 +1,27 @@
-import typing as ty
+import enum
 
 import vkquick.base.filter
 import vkquick.events_generators.event
 
 
+class ChatOnlyStatus(vkquick.base.filter.DecisionStatus):
+    PASSED = enum.auto()
+    NOT_PASSED = enum.auto()
+
+
 class ChatOnly(vkquick.base.filter.Filter):
 
-    passed_decision = "Сообщение отправлено с чата"
-    not_passed_decision = "Сообщение не отправлено с чата"
+    passed_decision = vkquick.base.filter.Decision(True, "Сообщение отправлено в чат")
+    not_passed_decision = vkquick.base.filter.Decision(False, "Сообщение отправлено в личные сообщения")
+    passed_response = vkquick.base.filter.FilterResponse(ChatOnlyStatus.PASSED, passed_decision)
+    not_passed_response = vkquick.base.filter.FilterResponse(ChatOnlyStatus.NOT_PASSED, not_passed_decision)
 
     def make_decision(
         self, event: vkquick.events_generators.event.Event
-    ) -> ty.Tuple[bool, str]:
+    ) -> vkquick.base.filter.FilterResponse:
+        """
+        Определяет откуда отправлено сообщение
+        """
         if event.get_message_object().peer_id > vkquick.peer(0):
-            return True, self.passed_decision
-        return False, self.not_passed_decision
+            return self.passed_response
+        return self.not_passed_response
