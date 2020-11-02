777368d35e5e7594ea02ce9ef899ac12964aa56c
HateMe02 <godmodeon34567@gmail.com>
2020-11-01 22:21:10 -0800

    Update Action filter to new syntax

--- vkquick/event_handling/filters/action.py
+++ vkquick/event_handling/filters/action.py
@@ -1,24 +1,31 @@
-import typing as ty
+import enum
 
 import vkquick.base.filter
 import vkquick.events_generators.event
 
 
+class ActionStatus(vkquick.base.filter.DecisionStatus):
+    PASSED = enum.auto()
+    NOT_PASSED = enum.auto()
+
+
 class Action(vkquick.base.filter.Filter):
 
-    passed_decision = "В сообщение содержатся нужное действие"
-    not_passed_decision = "В сообщение не содержатся нужное действие"
+    passed_decision = vkquick.base.filter.Decision(True, "В сообщение содержатся нужное действие")
+    not_passed_decision = vkquick.base.filter.Decision(False, "В сообщение не содержатся нужное действие")
+    passed_response = vkquick.base.filter.FilterResponse(ActionStatus.PASSED, passed_decision)
+    not_passed_response = vkquick.base.filter.FilterResponse(ActionStatus.NOT_PASSED, not_passed_decision)
 
     def __init__(self, *types):
         self.types = types
 
     def make_decision(
         self, event: vkquick.events_generators.event.Event
-    ) -> ty.Tuple[bool, str]:
+    ) -> vkquick.base.filter.FilterResponse:
         """
         Проверяет на действие в сообщение
         """
         message = event.get_message_object()
         if "action" in message and message.action.type in self.types:
-            return True, self.passed_decision
-        return False, self.not_passed_decision
+            return self.passed_response
+        return self.not_passed_response
