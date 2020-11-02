fbfba88faa573aa868e4f366ae6f714e3787d5ca
HateMe02 <godmodeon34567@gmail.com>
2020-11-01 22:21:40 -0800

    Update DirectOnly filter to new syntax

--- vkquick/event_handling/filters/direct_only.py
+++ vkquick/event_handling/filters/direct_only.py
@@ -1,29 +1,27 @@
-import typing as ty
+import enum
 
 import vkquick.base.filter
 import vkquick.events_generators.event
 
 
-class DirectOnlyDecision(vkquick.base.filter.DecisionEnum):
-    DIRECT = vkquick.base.filter.Decision(
-        "Сообщение отправлено в личные сообщения"
-    )
-    not_passed_decision = vkquick.base.filter.Decision(
-        "Сообщение не отправлено в личные сообщения"
-    )
+class DirectOnlyStatus(vkquick.base.filter.DecisionStatus):
+    PASSED = enum.auto()
+    NOT_PASSED = enum.auto()
 
 
 class DirectOnly(vkquick.base.filter.Filter):
 
-    passed_decision = "Сообщение отправлено в личные сообщения"
-    not_passed_decision = "Сообщение не отправлено в личные сообщения"
+    passed_decision = vkquick.base.filter.Decision(True, "Сообщение отправлено в личные сообщения")
+    not_passed_decision = vkquick.base.filter.Decision(False, "Сообщение не отправлено в личные сообщения")
+    passed_response = vkquick.base.filter.FilterResponse(DirectOnlyStatus.PASSED, passed_decision)
+    not_passed_response = vkquick.base.filter.FilterResponse(DirectOnlyStatus.NOT_PASSED, not_passed_decision)
 
     def make_decision(
         self, event: vkquick.events_generators.event.Event
-    ) -> ty.Tuple[bool, str]:
+    ) -> vkquick.base.filter.FilterResponse:
         """
         Определяет откуда отправлено сообщение
         """
         if event.get_message_object().peer_id < vkquick.peer(0):
-            return True, self.passed_decision
-        return False, self.not_passed_decision
+            return self.passed_response
+        return self.not_passed_response
