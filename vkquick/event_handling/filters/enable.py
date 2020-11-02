d0ee98186e14cac7801e43b49e3127f37ffff7f8
HateMe02 <godmodeon34567@gmail.com>
2020-11-01 22:22:09 -0800

    Remove unused import

--- vkquick/event_handling/filters/enable.py
+++ vkquick/event_handling/filters/enable.py
@@ -2,7 +2,6 @@ import enum
 
 import vkquick.base.filter
 import vkquick.events_generators.event
-import vkquick.utils
 
 
 class EnableStatus(vkquick.base.filter.DecisionStatus):
@@ -12,19 +11,10 @@ class EnableStatus(vkquick.base.filter.DecisionStatus):
 
 class Enable(vkquick.base.filter.Filter):
 
-    enabled_decision = vkquick.base.filter.Decision(
-        True, "Обработка команды включена"
-    )
-    disabled_decision = vkquick.base.filter.Decision(
-        False, "Обработка команды отключена"
-    )
-
-    enabled_response = vkquick.base.filter.FilterResponse(
-        EnableStatus.ENABLED, enabled_decision
-    )
-    disabled_response = vkquick.base.filter.FilterResponse(
-        EnableStatus.DISABLED, disabled_decision
-    )
+    enabled_decision = vkquick.base.filter.Decision(True, "Обработка команды включена")
+    disabled_decision = vkquick.base.filter.Decision(False, "Обработка команды отключена")
+    enabled_response = vkquick.base.filter.FilterResponse(EnableStatus.ENABLED, enabled_decision)
+    disabled_response = vkquick.base.filter.FilterResponse(EnableStatus.DISABLED, disabled_decision)
 
     def __init__(self, enabled: bool = True):
         self.enabled = enabled
@@ -32,6 +22,9 @@ class Enable(vkquick.base.filter.Filter):
     def make_decision(
         self, event: vkquick.events_generators.event.Event
     ) -> vkquick.base.filter.FilterResponse:
+        """
+        Задается вами для выключения/включения команды
+        """
         if self.enabled:
             return self.enabled_response
         return self.disabled_response
