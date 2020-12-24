fe9f86dc3c0581408dc174fed0f8eb15a246186f
Rhinik <deknowny@gmail.com>
2020-12-24 20:33:11 +0300

    Message fields refactor

--- vkquick/wrappers/message.py
+++ vkquick/wrappers/message.py
@@ -8,41 +8,14 @@ from vkquick.utils import AttrDict
 from vkquick.base.wrapper import Wrapper
 
 
-"""
-id: int
-date: datetime.datetime
-peer_id: int
-from_id: int
-text: str
-random_id: int
-attachments: ty.List[AttrDict]
-important: bool
-is_hidden: bool
-out: bool
-conversation_message_id: int
-keyboard: pydantic.Json
-fwd_messages: list
-geo: ty.Optional[AttrDict]
-payload: ty.Optional[pydantic.Json]
-reply_message: ty.Optional[AttrDict]
-action: ty.Optional[AttrDict]
-ref: ty.Optional[str]
-ref_source: ty.Optional[str]
-expire_ttl: ty.Optional[int]
-"""
-
-
 class Message(Wrapper):
-
     @property
     def id(self) -> int:
         return self.fields.id
 
     @functools.cached_property
     def date(self) -> datetime.datetime:
-        return datetime.datetime.fromtimestamp(
-            self.fields.date
-        )
+        return datetime.datetime.fromtimestamp(self.fields.date)
 
     @property
     def peer_id(self) -> int:
@@ -62,7 +35,7 @@ class Message(Wrapper):
 
     @property
     def attachments(self) -> ty.List[AttrDict]:
-        return self.fields.attachments
+        return list(self.fields.attachments)
 
     @property
     def important(self) -> bool:
@@ -82,11 +55,17 @@ class Message(Wrapper):
 
     @functools.cached_property
     def keyboard(self) -> ty.Optional[AttrDict]:
-        return AttrDict(json.loads(self.fields.keyboard)) if "keyboard" in self.fields else None
+        if "keyboard" in self.fields:
+            return AttrDict(
+                json.loads(self.fields.keyboard)
+            )
+        return None
 
     @functools.cached_property
     def fwd_messages(self) -> ty.List[Message]:
-        return list(map(self.__class__, self.fields.fwd_messages))
+        return list(
+            map(self.__class__, self.fields.fwd_messages)
+        )
 
     @property
     def geo(self) -> ty.Optional[AttrDict]:
@@ -94,15 +73,19 @@ class Message(Wrapper):
 
     @functools.cached_property
     def payload(self) -> ty.Optional[AttrDict]:
-        return AttrDict(json.loads(self.fields.payload)) if "payload" in self.fields else None
+        if "payload" in self.fields:
+            return AttrDict(json.loads(self.fields.payload))
+        return None
 
     @functools.cached_property
     def reply_message(self) -> ty.Optional[Message]:
-        return self.__class__(self.fields.reply_message) if "reply_message" in self.fields else None
+        if "reply_message" in self.fields:
+            return self.__class__(self.fields.reply_message)
+        return None
 
     @property
     def action(self) -> ty.Optional[AttrDict]:
-        return  self.fields.action if "action" in self.fields else None
+        return self.fields.action if "action" in self.fields else None
 
     @property
     def ref(self) -> ty.Optional[str]:
