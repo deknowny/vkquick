bc694af9c98d94914f169d6a4655117bc03af71f
HateMe02 <godmodeon34567@gmail.com>
2020-12-24 16:20:31 +0300

    Extend message fields

--- vkquick/wrappers/message.py
+++ vkquick/wrappers/message.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 import datetime
 import functools
-from vkquick.utils import AttrDict
+import json
 import typing as ty
 
+from vkquick.utils import AttrDict
 from vkquick.base.wrapper import Wrapper
 
 
@@ -32,17 +33,10 @@ expire_ttl: ty.Optional[int]
 
 
 class Message(Wrapper):
-    @property
-    def text(self) -> str:
-        return self.fields.text
 
     @property
-    def peer_id(self) -> int:
-        return self.fields.peer_id
-
-    @property
-    def from_id(self):
-        return self.fields.from_id
+    def id(self) -> int:
+        return self.fields.id
 
     @functools.cached_property
     def date(self) -> datetime.datetime:
@@ -51,17 +45,73 @@ class Message(Wrapper):
         )
 
     @property
-    def conversation_message_id(self) -> int:
-        return self.fields.conversation_message_id
+    def peer_id(self) -> int:
+        return self.fields.peer_id
 
     @property
-    def id(self) -> int:
-        return self.fields.id
+    def from_id(self) -> int:
+        return self.fields.from_id
+
+    @property
+    def text(self) -> str:
+        return self.fields.text
+
+    @property
+    def random_id(self) -> int:
+        return self.fields.random_id
+
+    @property
+    def attachments(self) -> ty.List[AttrDict]:
+        return self.fields.attachments
+
+    @property
+    def important(self) -> bool:
+        return bool(self.fields.important)
+
+    @property
+    def is_hidden(self) -> bool:
+        return bool(self.fields.important)
 
     @property
     def out(self) -> bool:
         return bool(self.fields.out)
 
     @property
-    def attachments(self) -> ty.List[AttrDict]:
+    def conversation_message_id(self) -> int:
         return self.fields.conversation_message_id
+
+    @functools.cached_property
+    def keyboard(self) -> ty.Optional[AttrDict]:
+        return AttrDict(json.loads(self.fields.keyboard)) if "keyboard" in self.fields else None
+
+    @functools.cached_property
+    def fwd_messages(self) -> ty.List[Message]:
+        return list(map(self.__class__, self.fields.fwd_messages))
+
+    @property
+    def geo(self) -> ty.Optional[AttrDict]:
+        return self.fields.geo if "geo" in self.fields else None
+
+    @functools.cached_property
+    def payload(self) -> ty.Optional[AttrDict]:
+        return AttrDict(json.loads(self.fields.payload)) if "payload" in self.fields else None
+
+    @functools.cached_property
+    def reply_message(self) -> ty.Optional[Message]:
+        return self.__class__(self.fields.reply_message) if "reply_message" in self.fields else None
+
+    @property
+    def action(self) -> ty.Optional[AttrDict]:
+        return  self.fields.action if "action" in self.fields else None
+
+    @property
+    def ref(self) -> ty.Optional[str]:
+        return self.fields.ref if "ref" in self.fields else None
+
+    @property
+    def ref_source(self) -> ty.Optional[str]:
+        return self.fields.ref_source if "ref_source" in self.fields else None
+
+    @property
+    def expire_ttl(self) -> ty.Optional[int]:
+        return self.fields.expire_ttl if "expire_ttl" in self.fields else None
