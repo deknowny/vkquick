f92d2e9b147e7ef8b47be18dd10ca3f11a80e70b
HateMe02 <godmodeon34567@gmail.com>
2020-12-30 11:51:11 +0300

    Add download methods

--- vkquick/wrappers/attachment.py
+++ vkquick/wrappers/attachment.py
@@ -1,10 +1,25 @@
 from __future__ import annotations
 
+import io
+
 from vkquick.base.serializable import Attachment
+from vkquick.utils import download_file
 
 
 class Photo(Attachment):
     _name = "photo"
+
+    async def download_min_size(self) -> bytes:
+        return await download_file(self.fields.sizes[-1].url)
+
+    async def download_with_size(self, size: chr) -> bytes:
+        for photo_size in self.fields.sizes:
+            if photo_size.type == size:
+                return await download_file(photo_size.url)
+        raise ValueError(f"There isn’t a size `{size}`")
+
+    async def download_max_size(self):
+        return await download_file(self.fields.sizes[0].url)
 
 
 class Document(Attachment):
