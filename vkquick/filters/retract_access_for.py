f58d1514ab9d37ce8908d45e2b6e2508c3f92a4a
HateMe02 <godmodeon34567@gmail.com>
2021-01-07 20:55:12 +0300

    Fix check. Issue #55

--- vkquick/filters/retract_access_for.py
+++ vkquick/filters/retract_access_for.py
@@ -1,6 +1,8 @@
 """
 RetractAccessFor фильтр
 """
+import typing as ty
+
 from vkquick.context import Context
 from vkquick.base.filter import Filter, Decision
 
@@ -17,10 +19,13 @@ class RetractAccessFor(Filter):
         False, "Пользователь не может пользоваться командой"
     )
 
-    def __init__(self, *ids):
+    def __init__(self, *ids, output: ty.Optional[bool] = None):
         self.ids = ids
+        self.output = output
 
     def make_decision(self, context: Context) -> Decision:
-        if context.msg.from_id not in self.ids:
+        if context.msg.from_id not in self.ids or (
+            self.output is not None and not context.msg.out
+        ):
             return self.passed_decision
         return self.not_passed_decision
