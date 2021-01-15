bf865a901edffe36711a7574d074526a476c34ba
Rhinik <deknowny@gmail.com>
2021-01-16 00:31:05 +0300

    Add a part of arguments explanation

---
+++ docs_src/command_text_args_many_args.py
@@ -0,0 +1,16 @@
+import vkquick as vq
+
+
+bot = vq.Bot.init_via_token("token")
+
+
+@bot.add_command(
+    prefixes="/"
+    names="add"
+)
+def foo(name: vq.Word, age: vq.Integer):
+    # Какие-то манипуляции...
+    return f"Пользователь с именем `{name}` и возрастом `{age}` добавлен!"
+
+
+bot.run()
---
+++ docs_src/command_text_args_simple.py
@@ -0,0 +1,15 @@
+import vkquick as vq
+
+
+bot = vq.Bot.init_via_token("token")
+
+
+@bot.add_command(
+    prefixes="/"
+    names="foo"
+)
+def foo(num: vq.Integer):
+    return f"Команда вызвана с числом {num}"
+
+
+bot.run()
