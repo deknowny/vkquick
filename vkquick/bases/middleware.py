0b402a9808660a6b27e70e3e31d5294f4d34d775
Rhinik <deknowny@gmail.com>
2021-03-28 20:51:07 +0300

    Add docstring to middlwares

--- vkquick/bases/middleware.py
+++ vkquick/bases/middleware.py
@@ -1,18 +1,30 @@
 from __future__ import annotations
 
-import abc
 import typing as ty
 
 if ty.TYPE_CHECKING:
     from vkquick.bot import EventProcessingContext
 
 
-class Middleware(abc.ABC):
-    """ """
-    @abc.abstractmethod
+class Middleware:
+    """
+    Мидлвар — сущность, содержащая методы, которые
+    будут вызваны перед тем, как начать обработку
+    события обработчиками событий и после того,
+    как все обработчики события были вызваны
+    """
     async def foreword(self, epctx: EventProcessingContext) -> None:
-        ...
+        """
+        Вызывается перед тем, как начать обработку
 
-    @abc.abstractmethod
+        Arguments:
+            epctx: Контекст процесса обработки события
+        """
+
     async def afterword(self, epctx: EventProcessingContext) -> None:
-        ...
+        """
+        Вызывается после того, как закончится обработка события
+
+        Arguments:
+            epctx: Контекст процесса обработки события
+        """
