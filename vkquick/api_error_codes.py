ecede00426e903f27082ac018f56e84b359c3f74
Rhinik <deknowny@gmail.com>
2021-05-25 12:23:05 +0300

    Add API error codes codogen

---
+++ vkquick/api_error_codes.py
@@ -0,0 +1,992 @@
+CODE_1_UNKNOWN = 1
+"""
+Unknown error occurred
+"""
+
+CODE_2_DISABLED = 2
+"""
+Application is disabled. Enable your application or use test mode
+"""
+
+CODE_3_METHOD = 3
+"""
+Unknown method passed
+"""
+
+CODE_4_SIGNATURE = 4
+"""
+Incorrect signature
+"""
+
+CODE_5_AUTH = 5
+"""
+User authorization failed
+"""
+
+CODE_6_TOO_MANY = 6
+"""
+Too many requests per second
+"""
+
+CODE_7_PERMISSION = 7
+"""
+Permission to perform this action is denied
+"""
+
+CODE_8_REQUEST = 8
+"""
+Invalid request
+"""
+
+CODE_9_FLOOD = 9
+"""
+Flood control
+"""
+
+CODE_10_SERVER = 10
+"""
+Internal server error
+"""
+
+CODE_11_ENABLED_IN_TEST = 11
+"""
+In test mode application should be disabled or user should be authorized
+"""
+
+CODE_12_COMPILE = 12
+"""
+Unable to compile code
+"""
+
+CODE_13_RUNTIME = 13
+"""
+Runtime error occurred during code invocation
+"""
+
+CODE_14_CAPTCHA = 14
+"""
+Captcha needed
+"""
+
+CODE_15_ACCESS = 15
+"""
+Access denied
+"""
+
+CODE_16_AUTH_HTTPS = 16
+"""
+HTTP authorization failed
+"""
+
+CODE_17_AUTH_VALIDATION = 17
+"""
+Validation required
+"""
+
+CODE_18_USER_DELETED = 18
+"""
+User was deleted or banned
+"""
+
+CODE_19_BLOCKED = 19
+"""
+Content blocked
+"""
+
+CODE_20_METHOD_PERMISSION = 20
+"""
+Permission to perform this action is denied for non-standalone applications
+"""
+
+CODE_21_METHOD_ADS = 21
+"""
+Permission to perform this action is allowed only for standalone and OpenAPI applications
+"""
+
+CODE_22_UPLOAD = 22
+"""
+Upload error
+"""
+
+CODE_23_METHOD_DISABLED = 23
+"""
+This method was disabled
+"""
+
+CODE_24_NEED_CONFIRMATION = 24
+"""
+Confirmation required
+"""
+
+CODE_25_NEED_TOKEN_CONFIRMATION = 25
+"""
+Token confirmation required
+"""
+
+CODE_27_GROUP_AUTH = 27
+"""
+Group authorization failed
+"""
+
+CODE_28_APP_AUTH = 28
+"""
+Application authorization failed
+"""
+
+CODE_29_RATE_LIMIT = 29
+"""
+Rate limit reached
+"""
+
+CODE_30_PRIVATE_PROFILE = 30
+"""
+This profile is private
+"""
+
+CODE_33_NOT_IMPLEMENTED_YET = 33
+"""
+Not implemented yet
+"""
+
+CODE_35_CLIENT_UPDATE_NEEDED = 35
+"""
+Client update needed
+"""
+
+CODE_36_TIMEOUT = 36
+"""
+Method execution was interrupted due to timeout
+"""
+
+CODE_37_USER_BANNED = 37
+"""
+User was banned
+"""
+
+CODE_38_UNKNOWN_APPLICATION = 38
+"""
+Unknown application
+"""
+
+CODE_39_UNKNOWN_USER = 39
+"""
+Unknown user
+"""
+
+CODE_40_UNKNOWN_GROUP = 40
+"""
+Unknown group
+"""
+
+CODE_41_ADDITIONAL_SIGNUP_REQUIRED = 41
+"""
+Additional signup required
+"""
+
+CODE_100_PARAM = 100
+"""
+One of the parameters specified was missing or invalid
+"""
+
+CODE_101_PARAM_API_ID = 101
+"""
+Invalid application API ID
+"""
+
+CODE_103_LIMITS = 103
+"""
+Out of limits
+"""
+
+CODE_104_NOT_FOUND = 104
+"""
+Not found
+"""
+
+CODE_105_SAVE_FILE = 105
+"""
+Couldn't save file
+"""
+
+CODE_106_ACTION_FAILED = 106
+"""
+Unable to process action
+"""
+
+CODE_113_PARAM_USER_ID = 113
+"""
+Invalid user id
+"""
+
+CODE_114_PARAM_ALBUM_ID = 114
+"""
+Invalid album id
+"""
+
+CODE_118_PARAM_SERVER = 118
+"""
+Invalid server
+"""
+
+CODE_119_PARAM_TITLE = 119
+"""
+Invalid title
+"""
+
+CODE_121_PARAM_HASH = 121
+"""
+Invalid hash
+"""
+
+CODE_122_PARAM_PHOTOS = 122
+"""
+Invalid photos
+"""
+
+CODE_125_PARAM_GROUP_ID = 125
+"""
+Invalid group id
+"""
+
+CODE_129_PARAM_PHOTO = 129
+"""
+Invalid photo
+"""
+
+CODE_140_PARAM_PAGE_ID = 140
+"""
+Page not found
+"""
+
+CODE_141_ACCESS_PAGE = 141
+"""
+Access to page denied
+"""
+
+CODE_146_MOBILE_NOT_ACTIVATED = 146
+"""
+The mobile number of the user is unknown
+"""
+
+CODE_147_INSUFFICIENT_FUNDS = 147
+"""
+Application has insufficient funds
+"""
+
+CODE_148_ACCESS_MENU = 148
+"""
+Access to the menu of the user denied
+"""
+
+CODE_150_PARAM_TIMESTAMP = 150
+"""
+Invalid timestamp
+"""
+
+CODE_171_FRIENDS_LIST_ID = 171
+"""
+Invalid list id
+"""
+
+CODE_173_FRIENDS_LIST_LIMIT = 173
+"""
+Reached the maximum number of lists
+"""
+
+CODE_174_FRIENDS_ADD_YOURSELF = 174
+"""
+Cannot add user himself as friend
+"""
+
+CODE_175_FRIENDS_ADD_IN_ENEMY = 175
+"""
+Cannot add this user to friends as they have put you on their blacklist
+"""
+
+CODE_176_FRIENDS_ADD_ENEMY = 176
+"""
+Cannot add this user to friends as you put him on blacklist
+"""
+
+CODE_177_FRIENDS_ADD_NOT_FOUND = 177
+"""
+Cannot add this user to friends as user not found
+"""
+
+CODE_180_PARAM_NOTE_ID = 180
+"""
+Note not found
+"""
+
+CODE_181_ACCESS_NOTE = 181
+"""
+Access to note denied
+"""
+
+CODE_182_ACCESS_NOTE_COMMENT = 182
+"""
+You can't comment this note
+"""
+
+CODE_183_ACCESS_COMMENT = 183
+"""
+Access to comment denied
+"""
+
+CODE_200_ACCESS_ALBUM = 200
+"""
+Access denied
+"""
+
+CODE_201_ACCESS_AUDIO = 201
+"""
+Access denied
+"""
+
+CODE_203_ACCESS_GROUP = 203
+"""
+Access to group denied
+"""
+
+CODE_204_ACCESS_VIDEO = 204
+"""
+Access denied
+"""
+
+CODE_205_ACCESS_MARKET = 205
+"""
+Access denied
+"""
+
+CODE_210_WALL_ACCESS_POST = 210
+"""
+Access to wall's post denied
+"""
+
+CODE_211_WALL_ACCESS_COMMENT = 211
+"""
+Access to wall's comment denied
+"""
+
+CODE_212_WALL_ACCESS_REPLIES = 212
+"""
+Access to post comments denied
+"""
+
+CODE_213_WALL_ACCESS_ADD_REPLY = 213
+"""
+Access to status replies denied
+"""
+
+CODE_214_WALL_ADD_POST = 214
+"""
+Access to adding post denied
+"""
+
+CODE_219_WALL_ADS_PUBLISHED = 219
+"""
+Advertisement post was recently added
+"""
+
+CODE_220_WALL_TOO_MANY_RECIPIENTS = 220
+"""
+Too many recipients
+"""
+
+CODE_221_STATUS_NO_AUDIO = 221
+"""
+User disabled track name broadcast
+"""
+
+CODE_222_WALL_LINKS_FORBIDDEN = 222
+"""
+Hyperlinks are forbidden
+"""
+
+CODE_223_WALL_REPLY_OWNER_FLOOD = 223
+"""
+Too many replies
+"""
+
+CODE_224_WALL_ADS_POST_LIMIT_REACHED = 224
+"""
+Too many ads posts
+"""
+
+CODE_225_WALL_DONUT = 225
+"""
+Donut is disabled
+"""
+
+CODE_232_LIKES_REACTION_CAN_NOT_BE_APPLIED = 232
+"""
+Reaction can not be applied to the object
+"""
+
+CODE_250_POLLS_ACCESS = 250
+"""
+Access to poll denied
+"""
+
+CODE_251_POLLS_POLL_ID = 251
+"""
+Invalid poll id
+"""
+
+CODE_252_POLLS_ANSWER_ID = 252
+"""
+Invalid answer id
+"""
+
+CODE_253_POLLS_ACCESS_WITHOUT_VOTE = 253
+"""
+Access denied, please vote first
+"""
+
+CODE_260_ACCESS_GROUPS = 260
+"""
+Access to the groups list is denied due to the user's privacy settings
+"""
+
+CODE_300_ALBUM_FULL = 300
+"""
+This album is full
+"""
+
+CODE_302_ALBUMS_LIMIT = 302
+"""
+Albums number limit is reached
+"""
+
+CODE_500_VOTES_PERMISSION = 500
+"""
+Permission denied. You must enable votes processing in application settings
+"""
+
+CODE_600_ADS_PERMISSION = 600
+"""
+Permission denied. You have no access to operations specified with given object(s)
+"""
+
+CODE_601_WEIGHTED_FLOOD = 601
+"""
+Permission denied. You have requested too many actions this day. Try later.
+"""
+
+CODE_602_ADS_PARTIAL_SUCCESS = 602
+"""
+Some part of the request has not been completed
+"""
+
+CODE_603_ADS_SPECIFIC = 603
+"""
+Some ads error occurs
+"""
+
+CODE_629_ADS_OBJECT_DELETED = 629
+"""
+Object deleted
+"""
+
+CODE_700_GROUP_CHANGE_CREATOR = 700
+"""
+Cannot edit creator role
+"""
+
+CODE_701_GROUP_NOT_IN_CLUB = 701
+"""
+User should be in club
+"""
+
+CODE_702_GROUP_TOO_MANY_OFFICERS = 702
+"""
+Too many officers in club
+"""
+
+CODE_703_GROUP_NEED_2FA = 703
+"""
+You need to enable 2FA for this action
+"""
+
+CODE_704_GROUP_HOST_NEED_2FA = 704
+"""
+User needs to enable 2FA for this action
+"""
+
+CODE_706_GROUP_TOO_MANY_ADDRESSES = 706
+"""
+Too many addresses in club
+"""
+
+CODE_711_GROUP_APP_IS_NOT_INSTALLED_IN_COMMUNITY = 711
+"""
+Application is not installed in community
+"""
+
+CODE_714_GROUP_INVITE_LINKS_NOT_VALID = 714
+"""
+Invite link is invalid - expired, deleted or not exists
+"""
+
+CODE_800_VIDEO_ALREADY_ADDED = 800
+"""
+This video is already added
+"""
+
+CODE_801_VIDEO_COMMENTS_CLOSED = 801
+"""
+Comments for this video are closed
+"""
+
+CODE_900_MESSAGES_USER_BLOCKED = 900
+"""
+Can't send messages for users from blacklist
+"""
+
+CODE_901_MESSAGES_DENY_SEND = 901
+"""
+Can't send messages for users without permission
+"""
+
+CODE_902_MESSAGES_PRIVACY = 902
+"""
+Can't send messages to this user due to their privacy settings
+"""
+
+CODE_907_MESSAGES_TOO_OLD_PTS = 907
+"""
+Value of ts or pts is too old
+"""
+
+CODE_908_MESSAGES_TOO_NEW_PTS = 908
+"""
+Value of ts or pts is too new
+"""
+
+CODE_909_MESSAGES_EDIT_EXPIRED = 909
+"""
+Can't edit this message, because it's too old
+"""
+
+CODE_910_MESSAGES_TOO_BIG = 910
+"""
+Can't sent this message, because it's too big
+"""
+
+CODE_911_MESSAGES_KEYBOARD_INVALID = 911
+"""
+Keyboard format is invalid
+"""
+
+CODE_912_MESSAGES_CHAT_BOT_FEATURE = 912
+"""
+This is a chat bot feature, change this status in settings
+"""
+
+CODE_913_MESSAGES_TOO_LONG_FORWARDS = 913
+"""
+Too many forwarded messages
+"""
+
+CODE_914_MESSAGES_TOO_LONG_MESSAGE = 914
+"""
+Message is too long
+"""
+
+CODE_917_MESSAGES_CHAT_USER_NO_ACCESS = 917
+"""
+You don't have access to this chat
+"""
+
+CODE_919_MESSAGES_CANT_SEE_INVITE_LINK = 919
+"""
+You can't see invite link for this chat
+"""
+
+CODE_920_MESSAGES_EDIT_KIND_DISALLOWED = 920
+"""
+Can't edit this kind of message
+"""
+
+CODE_921_MESSAGES_CANT_FWD = 921
+"""
+Can't forward these messages
+"""
+
+CODE_924_MESSAGES_CANT_DELETE_FOR_ALL = 924
+"""
+Can't delete this message for everybody
+"""
+
+CODE_925_MESSAGES_CHAT_NOT_ADMIN = 925
+"""
+You are not admin of this chat
+"""
+
+CODE_927_MESSAGES_CHAT_NOT_EXIST = 927
+"""
+Chat does not exist
+"""
+
+CODE_931_MESSAGES_CANT_CHANGE_INVITE_LINK = 931
+"""
+You can't change invite link for this chat
+"""
+
+CODE_932_MESSAGES_GROUP_PEER_ACCESS = 932
+"""
+Your community can't interact with this peer
+"""
+
+CODE_935_MESSAGES_CHAT_USER_NOT_IN_CHAT = 935
+"""
+User not found in chat
+"""
+
+CODE_936_MESSAGES_CONTACT_NOT_FOUND = 936
+"""
+Contact not found
+"""
+
+CODE_939_MESSAGES_MESSAGE_REQUEST_ALREADY_SENT = 939
+"""
+Message request already sent
+"""
+
+CODE_940_MESSAGES_TOO_MANY_POSTS = 940
+"""
+Too many posts in messages
+"""
+
+CODE_942_MESSAGES_CANT_PIN_ONE_TIME_STORY = 942
+"""
+Cannot pin one-time story
+"""
+
+CODE_943_MESSAGES_INTENT_CANT_USE = 943
+"""
+Cannot use this intent
+"""
+
+CODE_944_MESSAGES_INTENT_LIMIT_OVERFLOW = 944
+"""
+Limits overflow for this intent
+"""
+
+CODE_945_MESSAGES_CHAT_DISABLED = 945
+"""
+Chat was disabled
+"""
+
+CODE_946_MESSAGES_CHAT_UNSUPPORTED = 946
+"""
+Chat not supported
+"""
+
+CODE_947_MESSAGES_MEMBER_ACCESS_TO_GROUP_DENIED = 947
+"""
+Can't add user to chat, because user has no access to group
+"""
+
+CODE_949_MESSAGES_CANT_EDIT_PINNED_YET = 949
+"""
+Can't edit pinned message yet
+"""
+
+CODE_950_MESSAGES_PEER_BLOCKED_REASON_BY_TIME = 950
+"""
+Can't send message, reply timed out
+"""
+
+CODE_1105_AUTH_FLOOD_ERROR = 1105
+"""
+Too many auth attempts, try again later
+"""
+
+CODE_1150_PARAM_DOC_ID = 1150
+"""
+Invalid document id
+"""
+
+CODE_1151_PARAM_DOC_DELETE_ACCESS = 1151
+"""
+Access to document deleting is denied
+"""
+
+CODE_1152_PARAM_DOC_TITLE = 1152
+"""
+Invalid document title
+"""
+
+CODE_1153_PARAM_DOC_ACCESS = 1153
+"""
+Access to document is denied
+"""
+
+CODE_1160_PHOTO_CHANGED = 1160
+"""
+Original photo was changed
+"""
+
+CODE_1170_TOO_MANY_LISTS = 1170
+"""
+Too many feed lists
+"""
+
+CODE_1251_APPS_ALREADY_UNLOCKED = 1251
+"""
+This achievement is already unlocked
+"""
+
+CODE_1256_APPS_SUBSCRIPTION_NOT_FOUND = 1256
+"""
+Subscription not found
+"""
+
+CODE_1257_APPS_SUBSCRIPTION_INVALID_STATUS = 1257
+"""
+Subscription is in invalid status
+"""
+
+CODE_1260_INVALID_ADDRESS = 1260
+"""
+Invalid screen name
+"""
+
+CODE_1310_COMMUNITIES_CATALOG_DISABLED = 1310
+"""
+Catalog is not available for this user
+"""
+
+CODE_1311_COMMUNITIES_CATEGORIES_DISABLED = 1311
+"""
+Catalog categories are not available for this user
+"""
+
+CODE_1400_MARKET_RESTORE_TOO_LATE = 1400
+"""
+Too late for restore
+"""
+
+CODE_1401_MARKET_COMMENTS_CLOSED = 1401
+"""
+Comments for this market are closed
+"""
+
+CODE_1402_MARKET_ALBUM_NOT_FOUND = 1402
+"""
+Album not found
+"""
+
+CODE_1403_MARKET_ITEM_NOT_FOUND = 1403
+"""
+Item not found
+"""
+
+CODE_1404_MARKET_ITEM_ALREADY_ADDED = 1404
+"""
+Item already added to album
+"""
+
+CODE_1405_MARKET_TOO_MANY_ITEMS = 1405
+"""
+Too many items
+"""
+
+CODE_1406_MARKET_TOO_MANY_ITEMS_IN_ALBUM = 1406
+"""
+Too many items in album
+"""
+
+CODE_1407_MARKET_TOO_MANY_ALBUMS = 1407
+"""
+Too many albums
+"""
+
+CODE_1408_MARKET_ITEM_HAS_BAD_LINKS = 1408
+"""
+Item has bad links in description
+"""
+
+CODE_1409_MARKET_EXTENDED_NOT_ENABLED = 1409
+"""
+Extended market not enabled
+"""
+
+CODE_1412_MARKET_GROUPING_ITEMS_WITH_DIFFERENT_PROPERTIES = 1412
+"""
+Grouping items with different properties
+"""
+
+CODE_1413_MARKET_GROUPING_ALREADY_HAS_SUCH_VARIANT = 1413
+"""
+Grouping already has such variant
+"""
+
+CODE_1416_MARKET_VARIANT_NOT_FOUND = 1416
+"""
+Variant not found
+"""
+
+CODE_1417_MARKET_PROPERTY_NOT_FOUND = 1417
+"""
+Property not found
+"""
+
+CODE_1425_MARKET_GROUPING_MUST_CONTAIN_MORE_THAN_ONE_ITEM = 1425
+"""
+Grouping must have two or more items
+"""
+
+CODE_1426_MARKET_GROUPING_ITEMS_MUST_HAVE_DISTINCT_PROPERTIES = 1426
+"""
+Item must have distinct properties
+"""
+
+CODE_1427_MARKET_ORDERS_NO_CART_ITEMS = 1427
+"""
+Cart is empty
+"""
+
+CODE_1429_MARKET_INVALID_DIMENSIONS = 1429
+"""
+Specify width, length, height and weight all together
+"""
+
+CODE_1430_MARKET_CANT_CHANGE_VKPAY_STATUS = 1430
+"""
+VK Pay status can not be changed
+"""
+
+CODE_1431_MARKET_SHOP_ALREADY_ENABLED = 1431
+"""
+Market was already enabled in this group
+"""
+
+CODE_1432_MARKET_SHOP_ALREADY_DISABLED = 1432
+"""
+Market was already disabled in this group
+"""
+
+CODE_1433_MARKET_PHOTOS_CROP_INVALID_FORMAT = 1433
+"""
+Invalid image crop format
+"""
+
+CODE_1434_MARKET_PHOTOS_CROP_OVERFLOW = 1434
+"""
+Crop bottom right corner is outside of the image
+"""
+
+CODE_1435_MARKET_PHOTOS_CROP_SIZE_TOO_LOW = 1435
+"""
+Crop size is less than the minimum
+"""
+
+CODE_1438_MARKET_NOT_ENABLED = 1438
+"""
+Market not enabled
+"""
+
+CODE_1600_STORY_EXPIRED = 1600
+"""
+Story has already expired
+"""
+
+CODE_1602_STORY_INCORRECT_REPLY_PRIVACY = 1602
+"""
+Incorrect reply privacy
+"""
+
+CODE_1900_PRETTY_CARDS_CARD_NOT_FOUND = 1900
+"""
+Card not found
+"""
+
+CODE_1901_PRETTY_CARDS_TOO_MANY_CARDS = 1901
+"""
+Too many cards
+"""
+
+CODE_1902_PRETTY_CARDS_CARD_IS_CONNECTED_TO_POST = 1902
+"""
+Card is connected to post
+"""
+
+CODE_2000_CALLBACK_API_SERVERS_LIMIT = 2000
+"""
+Servers number limit is reached
+"""
+
+CODE_2100_STICKERS_NOT_PURCHASED = 2100
+"""
+Stickers are not purchased
+"""
+
+CODE_2101_STICKERS_TOO_MANY_FAVORITES = 2101
+"""
+Too many favorite stickers
+"""
+
+CODE_2102_STICKERS_NOT_FAVORITE = 2102
+"""
+Stickers are not favorite
+"""
+
+CODE_3102_WALL_CHECK_LINK_CANT_DETERMINE_SOURCE = 3102
+"""
+Specified link is incorrect (can't find source)
+"""
+
+CODE_3300_RECAPTCHA = 3300
+"""
+Recaptcha needed
+"""
+
+CODE_3301_PHONE_VALIDATION_NEED = 3301
+"""
+Phone validation needed
+"""
+
+CODE_3302_PASSWORD_VALIDATION_NEED = 3302
+"""
+Password validation needed
+"""
+
+CODE_3303_OTP_VALIDATION_NEED = 3303
+"""
+Otp app validation needed
+"""
+
+CODE_3304_EMAIL_CONFIRMATION_NEED = 3304
+"""
+Email confirmation needed
+"""
+
+CODE_3305_ASSERT_VOTES = 3305
+"""
+Assert votes
+"""
+
+CODE_3609_TOKEN_EXTENSION_REQUIRED = 3609
+"""
+Token extension required
+"""
+
+CODE_3610_USER_DEACTIVATED = 3610
+"""
+User is deactivated
+"""
+
+CODE_3611_USER_SERVICE_DEACTIVATED = 3611
+"""
+Service is deactivated for user
+"""
+
+CODE_3800_FAVE_ALIEXPRESS_TAG = 3800
+"""
+Can't set AliExpress tag to this type of object
+"""
+
+
+__all__ = [var for var in locals().keys() if not var.startswith("_")]
