7e182f7b6e72d9e11dba819e35da08c1b13306c4
Rhinik <deknowny@gmail.com>
2021-05-27 22:10:22 +0300

    Remove some docstrings

--- vkquick/json_parsers.py
+++ vkquick/json_parsers.py
@@ -19,98 +19,44 @@ except ImportError:  # pragma: no cover
 
 
 class BuiltinJsonParser(BaseJSONParser):
-    """JSON парсер, использующий стандартную библиотеку"""
+    """
+    JSON парсер, использующий стандартную библиотеку
+    """
 
     @staticmethod
     def dumps(data: ty.Dict[str, ty.Any]) -> ty.Union[str, bytes]:
-        """
-
-        Args:
-          data: ty.Dict[str:
-          ty.Any]:
-          data: ty.Dict[str:
-
-        Returns:
-
-        """
         return json.dumps(data, ensure_ascii=False, separators=(",", ":"))
 
     @staticmethod
     def loads(string: ty.Union[str, bytes]) -> ty.Any:
-        """
-
-        Args:
-          string: ty.Union[str:
-          bytes]:
-          string: ty.Union[str:
-
-        Returns:
-
-        """
         return json.loads(string)
 
 
 class OrjsonParser(BaseJSONParser):
-    """JSON парсер, использующий `orjson`"""
+    """
+    JSON парсер, использующий `orjson`
+    """
 
     @staticmethod
     def dumps(data: ty.Dict[str, ty.Any]) -> ty.Union[str, bytes]:
-        """
-
-        Args:
-          data: ty.Dict[str:
-          ty.Any]:
-          data: ty.Dict[str:
-
-        Returns:
-
-        """
         return orjson.dumps(data)  # pragma: no cover
 
     @staticmethod
     def loads(string: ty.Union[str, bytes]) -> ty.Any:
-        """
-
-        Args:
-          string: ty.Union[str:
-          bytes]:
-          string: ty.Union[str:
-
-        Returns:
-
-        """
         return orjson.loads(string)  # pragma: no cover
 
 
 class UjsonParser(BaseJSONParser):
-    """JSON парсер, использующий `ujson`"""
+    """
+    JSON парсер, использующий `ujson`
+    """
 
     @staticmethod
     def dumps(data: ty.Dict[str, ty.Any]) -> ty.Union[str, bytes]:
-        """
-
-        Args:
-          data: ty.Dict[str:
-          ty.Any]:
-          data: ty.Dict[str:
-
-        Returns:
-
-        """
         return ujson.dumps(data, ensure_ascii=False)  # pragma: no cover
 
     @staticmethod
     def loads(string: ty.Union[str, bytes]) -> ty.Any:
-        """
-
-        Args:
-          string: ty.Union[str:
-          bytes]:
-          string: ty.Union[str:
-
-        Returns:
-
-        """
         return ujson.loads(string)  # pragma: no cover
 
 
